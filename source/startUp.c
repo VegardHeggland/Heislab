9358bdec870b546bb082ad449515c14eab491777
Vegard Heggland <vegarmhe@stud.ntnu.no>
2024-02-22 19:10:49 +0100

    Fix startUp()
    Made the startUp() function shorter and easier.

--- source/startUp.c
+++ source/startUp.c
@@ -2,13 +2,8 @@
 #include "driver/elevio.h"
 
 void elevatorStartUp() {
-    int floor;
-    while(1) {
+    int currentFloor = -1;
+    while(currentFloor == -1) {
         elevio_motorDirection(DIRN_UP);
-        floor = elevio_floorSensor();
-        if(floor== 0 || floor== 1 || floor == 2 || floor ==3) {
-            elevio_motorDirection(DIRN_STOP);
-            break;
-        } 
     }    
 }
