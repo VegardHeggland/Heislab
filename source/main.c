7b27cd1e01c934774a2e83635e9a72f7282d55c2
Vegard Heggland <vegarmhe@stud.ntnu.no>
2024-02-29 12:24:37 +0100

    Make addOrder() and removeOrder()

--- source/main.c
+++ source/main.c
@@ -5,21 +5,32 @@
 #include "driver/elevio.h"
 #include "startUp.h"
 #include "utilities.h"
+#include "queueSystem.h"
 
-
+extern Order queueWithOrders[10];
 
 int main(){
     elevio_init();
     elevatorStartUp();
+    printf("queueSystem: \n");
+    
+    for (int i = 0; i < MAX_ORDERS; i++) {
+        printf("Bestilling: currentFloor: %d, designatedFloor: %d, activeOrder?: %d\n" , queueWithOrders[i].currentFloor, queueWithOrders[i].designatedFloor, queueWithOrders[i].activeOrder);
+    } printf("\n");
 
-
-    while(1) {
-        
-
-
-
-
+    for (int i = 0; i < 8; i++) {
+        addOrder(1, 3, -1);
     }
-  
+
+   for (int i = 0; i < MAX_ORDERS; i++) {
+        printf("Bestilling: currentFloor: %d, designatedFloor: %d, activeOrder?: %d\n" , queueWithOrders[i].currentFloor, queueWithOrders[i].designatedFloor, queueWithOrders[i].activeOrder);
+    } printf("\n");
+    
+    removeOrder(6);
+    
+    for (int i = 0; i < MAX_ORDERS; i++) {
+        printf("Bestilling: currentFloor: %d, designatedFloor: %d, activeOrder?: %d\n" , queueWithOrders[i].currentFloor, queueWithOrders[i].designatedFloor, queueWithOrders[i].activeOrder);
+    } printf("\n");
+
     return 0;
 }
--- source/queueSystem.h
+++ source/queueSystem.h
@@ -1,10 +1,24 @@
 #pragma once
+#include "driver/elevio.h"
+#include "stdbool.h" 
 
+#define MAX_ORDERS 10
 
-typedef struct FloorSignals{};
-typedef struct Order{};
+typedef struct {
+    bool hallUpActivated;
+    bool hallDownActivated;
+    bool inCabOrder;
+} FloorSignals;
+
+typedef struct {
+    ButtonType btnType; 
+    int currentFloor;
+    int designatedFloor;
+    bool activeOrder;
+} Order; 
 
 void updateFloorSignals();
 
-void addOrder(); //Legg til ordre bakerst i listen.  Parameter eller sjekke mot signalMatrkse?
+void addOrder(ButtonType inputBtn, int inputFloor, int inputDesignatedFloor); //Legg til ordre bakerst i listen.  Parameter eller sjekke mot signalMatrkse?
+
 void removeOrder(int index); //iterere gjennom liste og fjerne ordre på indeks = teller under iterering? INkludere dytting av listen fremover
